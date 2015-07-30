019f0cdc73f3d6388284b047e1320c54335c8be9
chlachof <chlachof@126.com>
2015-07-30 15:11:47 +0800

    修改英文注释为中文，增加了内存对齐，MCB没有对齐内存是个重大失误

--- Core/MemoryControllerBlock/mMemoryControllerBlock.c
+++ Core/MemoryControllerBlock/mMemoryControllerBlock.c
@@ -1,40 +1,53 @@
+/*
+chlachof@126.com
+2015.7.30
+*/
 #include "mMemoryControllerBlock.h"
+#include "stdlib.h"
 
-const char      g_pchFree[5] = "free";
-const char      g_pchNA[4] = "N/A";
-unsigned char   g_pucMCB[MCB_SIZE] = {0};
-struct MemoryBlockList * g_pstMemoryBlock = NULL;
+const char          g_pchFree[5] = "free";
+const char          g_pchNA[4] = "N/A";
+union MemoryHeap    g_unHeap;
+
+struct MemoryBlockList * g_pstMemoryBlock = (struct MemoryBlockList *)g_unHeap.pucMCB;
 
 /*
-as same as malloc(size), and add a parameter called "name" for showing
+函数功能：同malloc(size)，增加name参数，用于在shell中展示僵尸碎片，当然不使用名字传入null也可以，默认名字为"N/A"
+输入：内存块名字(char *)，内存块大小(unsigned long)
+返回：内存块地址(void *)
 */
 void * mMalloc (char * pchName, unsigned long ulSize)
 {
     struct MemoryBlockList * pstMemoryTemp = NULL;
     struct MemoryBlockList * pstMemoryInsert = NULL;
     
-    if(m_ulSize <= 0 || ulSize > MCB_SIZE) 
+    if(ulSize <= 0 || ulSize > MCB_SIZE) 
     {
         return NULL;
     }
     
+    if(ulSize & MCB_ALIGNMENT_MASK)
+    {
+        ulSize += (MCB_ALIGNMENT - (ulSize & MCB_ALIGNMENT_MASK));
+    }
+    
     /*
-    init g_pstMemoryBlock
+    初始化内存块头节点
     */
-    if(g_pstMemoryBlock == NULL)
+    if(g_pstMemoryBlock->m_pchName == NULL)
     {
         if(MCB_SIZE < sizeof(struct MemoryBlockList))
         {
             return NULL;
         }
-        g_pstMemoryBlock = g_pucMCB;
+        memset(g_pstMemoryBlock, 0, MCB_SIZE);
         g_pstMemoryBlock->m_pchName = g_pchFree;
         g_pstMemoryBlock->m_ulSize = MCB_SIZE - sizeof(struct MemoryBlockList);
         g_pstMemoryBlock->m_pstNext = NULL;
     }
     
     /*
-    select suitable block for the newcomer. the smallest but bigger than the newcomer
+    为新创建的内存块分配合适的碎片空间，是所有比内存块大中最小的
     */
     pstMemoryTemp = g_pstMemoryBlock;
     
@@ -42,7 +55,7 @@ void * mMalloc (char * pchName, unsigned long ulSize)
     {
         if(pstMemoryTemp->m_pchName == g_pchFree)
         {
-            if(pstMemoryTemp->m_ulSize > (ulSize + sizeof(struct MemoryBlockList)))
+            if(pstMemoryTemp->m_ulSize >= ulSize)
             {
                 if(pstMemoryInsert == NULL)
                 {
@@ -67,11 +80,11 @@ void * mMalloc (char * pchName, unsigned long ulSize)
     
     if(pstMemoryInsert->m_ulSize > (ulSize + 2*sizeof(struct MemoryBlockList)))
     {
-        pstMemoryTemp = pstMemoryInsert + pstMemoryInsert->m_ulSize - ulSize;
+        pstMemoryTemp = (struct MemoryBlockList *)((char *)pstMemoryInsert + pstMemoryInsert->m_ulSize - ulSize);
         pstMemoryTemp->m_ulSize = ulSize;
         pstMemoryTemp->m_pstNext = pstMemoryInsert->m_pstNext;
         
-        pstMemoryInsert->ulSize -= (ulSize + sizeof(struct MemoryBlockList));
+        pstMemoryInsert->m_ulSize -= (ulSize + sizeof(struct MemoryBlockList));
         pstMemoryInsert->m_pstNext = pstMemoryTemp;
     }
     else 
@@ -88,11 +101,14 @@ void * mMalloc (char * pchName, unsigned long ulSize)
         pstMemoryTemp->m_pchName = g_pchNA;
     }
     
-    return (pstMemoryTemp + sizeof(struct MemoryBlockList));
+    
+    return ((void *)((char *)pstMemoryTemp + sizeof(struct MemoryBlockList)));
 }
 
 /*
-as same as free(ptr)
+函数功能：同free(ptr)，释放内存块
+输入：内存块地址(void *)
+返回：无
 */
 void mFree (void * pvAddress)
 {
@@ -105,7 +121,7 @@ void mFree (void * pvAddress)
     {
         if(pstMemoryTemp->m_pchName != g_pchFree)
         {
-            if(pvAddress == (pstMemoryTemp + sizeof(struct MemoryBlockList)))
+            if(pvAddress == ((char *)pstMemoryTemp + sizeof(struct MemoryBlockList)))
             {
                 if(pstMemoryTempPrv->m_pchName == g_pchFree)
                 {
@@ -125,22 +141,33 @@ void mFree (void * pvAddress)
 }
 
 /*
-to make all free blocks as one
+函数功能：整理所有内存块，使所有标记为free的内存块，物理上、逻辑上合在一起
+输入：无
+返回：无
 */
-void mMemoryArrange (void)
+void mDefrag (void)
+{
+    
+}
+
+
+#ifdef _M_SHELL_
+/*
+函数功能：展示所有的内存块，这个函数只在shell中使用
+输入：无
+返回：无
+*/
+void mListBlock4Shell (void)
 {
 }
 
 /*
-list all blocks
+函数功能：使用名字来释放内存块，这个函数只在shell中使用
+输入：内存块名字(char *)
+返回：无
 */
-void mBlockList (void)
+void mFree4Shell (char * pchName)
 {
 }
 
-/*
-as same as mFree, but use name to free blocks, however, it is usually called by shell
-*/
-void mFreeByName (char * pchName)
-{
-}
+#endif
--- Core/inc/mMemoryControllerBlock.h
+++ Core/inc/mMemoryControllerBlock.h
@@ -1,17 +1,61 @@
-#ifndef _MCB_
-#define _MCB_
+#ifndef _M_MCB_
+#define _M_MCB_
 
 #ifndef MCB_SIZE
-#define MCB_SIZE            (10240)
+#define MCB_SIZE            (100)
 #endif
 
-#define BLOCK_NAME_LEN_MAX  (10)
+#define MCB_ALIGNMENT       (4)
+#define MCB_ALIGNMENT_MASK  (MCB_ALIGNMENT - 1)
 
 struct MemoryBlockList
 {
-    const char * m_pchName;
-    unsigned long m_ulSize;
-    struct MemoryBlockList * m_pstNext;
-}
+    unsigned long m_ulSize;             //内存块大小
+    const char * m_pchName;             //内存块名字
+    struct MemoryBlockList * m_pstNext; //next指针
+};
+
+union MemoryHeap
+{
+    volatile unsigned long  ulDummy;
+    unsigned char           pucMCB[MCB_SIZE]
+};
+
+/*
+函数功能：同malloc(size)，增加name参数，用于在shell中展示僵尸碎片，当然不使用名字传入null也可以，默认名字为"N/A"
+输入：内存块名字(char *)，内存块大小(unsigned long)
+返回：内存块地址(void *)
+*/
+void * mMalloc (char * pchName, unsigned long ulSize);
+
+/*
+函数功能：同free(ptr)，释放内存块
+输入：内存块地址(void *)
+返回：无
+*/
+void mFree (void * pvAddress);
+
+/*
+函数功能：整理所有内存块，使所有标记为free的内存块，物理上合在一起
+输入：无
+返回：无
+*/
+void mDefrag (void);
+
+#ifdef _M_SHELL_
+/*
+函数功能：展示所有的内存块，这个函数只在shell中使用
+输入：无
+返回：无
+*/
+void mListBlock4Shell (void);
+
+/*
+函数功能：使用名字来释放内存块，这个函数只在shell中使用
+输入：内存块名字(char *)
+返回：无
+*/
+void mFree4Shell (char * pchName);
+#endif
 
 #endif
